216e0a8f2b4df91f6ba0d4ac56b5fe2f5ca003c5
lszperling <lszperling@gmail.com>
2015-03-19 23:30:30 +0100

    Typo fix

--- justPlotIt.R
+++ justPlotIt.R
@@ -2,6 +2,6 @@ png(filename = "statsPlot.png",width = 1200, height = 800)
 elplot <- qplot(x= date/10000, y = porcYes ,data=dataset, color = Name, 
       geom = c("line", "point"), method = "lm", facets = ~Name, xlab = "Time", 
       ylab = "Percent of members that voted and voted 'Yes'", 
-      main = "Porcentege of members by Intergovernmental organization that voted 'Yes' through time") + theme(text = element_text(size=15), axis.text.x = element_text(size = 20), axis.text.y = element_text(size = 20), legend.text = element_text(size = 15))
+      main = "Percentege of members by Intergovernmental organization that voted 'Yes' through time") + theme(text = element_text(size=15), axis.text.x = element_text(size = 20), axis.text.y = element_text(size = 20), legend.text = element_text(size = 15))
 print(elplot)
 dev.off()
